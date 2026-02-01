4e9072803292c954fa48286b949fd15f0196cab8
mepha89 <umamarahman12@gmail.com>
2026-02-01 11:45:59 -0330

    refactored-hello-world-test

--- Learn_Go_with_tests/hello_world/hello.go
+++ Learn_Go_with_tests/hello_world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPreix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPreix + name
 }
 
 func main() {
--- Learn_Go_with_tests/hello_world/hello_test.go
+++ Learn_Go_with_tests/hello_world/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Umama")
-	want := "Hello, Umama"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Umama")
+		want := "Hello, Umama"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
