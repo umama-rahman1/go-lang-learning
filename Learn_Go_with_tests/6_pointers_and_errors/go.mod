e27b14ed40676e7899a7f7951116f1a662c2c913
umama-rahman1 <umamarahman12@gmail.com>
2026-03-29 11:22:38 +0300

    6-pointers-started

---
+++ Learn_Go_with_tests/6_pointers_and_errors/go.mod
@@ -0,0 +1,3 @@
+module example.com/pointers_and_errors
+
+go 1.25.6
---
+++ Learn_Go_with_tests/6_pointers_and_errors/wallet.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
