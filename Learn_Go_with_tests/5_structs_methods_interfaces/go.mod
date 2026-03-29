49bea0154982c7b56c24d019fb88ce5a5d794b84
umama-rahman1 <umamarahman12@gmail.com>
2026-03-29 10:09:31 +0300

    struct-running-basic-tests

---
+++ Learn_Go_with_tests/5_structs_methods_interfaces/go.mod
@@ -0,0 +1,3 @@
+module example.com/structs_methods
+
+go 1.25.6
---
+++ Learn_Go_with_tests/5_structs_methods_interfaces/shapes_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(5.0, 10.0)
+	want := 50.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
