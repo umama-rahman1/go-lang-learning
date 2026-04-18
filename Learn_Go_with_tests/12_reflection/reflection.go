7955c1566acf4b2b4c0ff112bf4356d8519c764a
umama-rahman1 <umamarahman12@gmail.com>
2026-04-18 11:21:31 -0230

    12-wip

--- Learn_Go_with_tests/12_reflection/reflection.go
+++ Learn_Go_with_tests/12_reflection/reflection.go
@@ -3,10 +3,26 @@ package main
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
